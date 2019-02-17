5916f2035ca28e76a4cbf14620d1247a79541157
guzhongren <guzhongren@live.cn>
2019-02-17 15:50:59 +0800

    feat(axis): 纵横坐标标注完成

--- src/Chart.ts
+++ src/Chart.ts
@@ -41,37 +41,67 @@ class Chart {
 		// 绘制纵坐标轴
 		const YStart = [option.canvas.offset, canvasSize[0] - option.canvas.offset]
 		const YEnd = [option.canvas.offset, option.canvas.offset]
+		this.context.textAlign = 'right'
 		this.drawAxis(YStart, YEnd)
 		// 绘制横坐标轴
 		const X = canvasSize[0] - option.canvas.offset
 		const XStart = [option.canvas.offset, X]
 		const XEnd = [canvasSize[1] - option.canvas.offset, X]
 		this.drawAxis(XStart, XEnd)
-		// TODO: 根据数据画柱状图
+		// 根据数据画柱状图
 		this.getRectArray(this.option, canvasSize).map((rect) => {
-			console.log(rect)
 			this.drawRect(rect.x, rect.y, rect.w, -rect.h, option.theme.color)
 		})
-		this.drawFont('ter', 200, 200, 'red', 30)
+		this.drawYLabels(option, canvasSize)
+		this.drawXLabels(option, canvasSize)
 		this.context.save()
 	}
+	drawXLabels(option: IOption, canvasSize: [number, number]) {
+		console.table(option)
+		const xAxis = option.data.xAxis
+		const halfWidth = this.getChartSize(option, canvasSize)[1] / xAxis.length / 2
+		const offset = option.canvas.offset
+		const y = canvasSize[0] - offset
+		xAxis.map((item: string, index: number) => {
+			const x = offset + (2 * index + 1) * halfWidth
+			this.context.textAlign = 'center'
+			this.drawText(item, x, y + this.option.canvas.offset / 2)
+		})
+	}
+	drawYLabels(option: IOption, canvasSize: [number, number]) {
+		const [maxValue, realNumberCount] = this.getRealNumCount(option.data.yAxis)
+		// 将纵坐标等分
+		const canvasOffset = option.canvas.offset
+		const halfCanvasOffset = canvasOffset / 4 * 3
+		const partCount = Math.ceil(maxValue / Math.pow(10, realNumberCount - 1))
+		const chartSize = this.getChartSize(option, canvasSize)
+		const partHeight = chartSize[0] / partCount
+		for (let i = 0; i <= partCount; i ++) {
+			const tempYHeight = canvasSize[0] - canvasOffset - partHeight * i
+			const text = (i * Math.pow(10, realNumberCount - 1)).toString()
+			this.drawText(text, canvasOffset - this.option.canvas.offset / 2, tempYHeight, '#000')
+			this.context.textAlign = 'right'
+			this.drawAxis([canvasOffset, tempYHeight], [halfCanvasOffset, tempYHeight])
+		}
+	}
+	/**
+	 * 获取每个柱状图数据的数组
+	 *
+	 * @param {IOption} option
+	 * @param {[number, number]} canvasSize
+	 * @returns
+	 * @memberof Chart
+	 */
 	getRectArray(option: IOption, canvasSize: [number, number]) {
 		const data = option.data
-		const chartSize = [canvasSize[0] - option.canvas.offset * 2, canvasSize[1] - option.canvas.offset * 2]
+		const chartSize = this.getChartSize(option, canvasSize)
 		const width = chartSize[1] / data.xAxis.length
 		// 柱状图空白部分
 		const offset = width * (option.chart.xOffset / 100)
-		// 处理纵坐标值
-		const maxValue = Math.max(...data.yAxis)
-		const maxValueString = maxValue.toString()
-		let realNumberCount = 0 // 实数部分的长度
-		if (maxValueString.indexOf('.') === -1) {
-			realNumberCount = maxValueString.length
-		} else {
-			realNumberCount = maxValueString.split('.')[0].length
-		}
+		const [maxValue, realNumberCount] = this.getRealNumCount(data.yAxis)
+		const partCount = Math.ceil(maxValue / Math.pow(10, realNumberCount - 1))
 		// y轴最大值的近似整十、整百数....
-		const yAxisMaxValue = Math.ceil(maxValue / Math.pow(10, realNumberCount - 1)) * Math.pow(10, realNumberCount - 1)
+		const yAxisMaxValue = partCount * Math.pow(10, realNumberCount - 1)
 		const rects = []
 		const yStart = chartSize[0] + option.canvas.offset
 		const blockWidth = width - (2 * offset)
@@ -87,6 +117,36 @@ class Chart {
 			rects.push(rect)
 		})
 		return rects
+	}
+	/**
+	 * 获取真实画图画布的大小
+	 *
+	 * @param {IOption} option
+	 * @param {[number, number]} canvasSize
+	 * @returns {[number, number]} [height, width]
+	 * @memberof Chart
+	 */
+	getChartSize(option: IOption, canvasSize: [number, number]): [number, number] {
+		const canvasOffset = option.canvas.offset
+		return [canvasSize[0] - canvasOffset * 2, canvasSize[1] - option.canvas.offset * 2]
+	}
+	/**
+	 * 获取数据的最大值和最大值的实数部分的长度
+	 *
+	 * @param {number[]} yAxisData
+	 * @returns {[number, number]}
+	 * @memberof Chart
+	 */
+	getRealNumCount(yAxisData: number[]): [number, number] {
+		const maxValue = Math.max(...yAxisData)
+		const maxValueString = maxValue.toString()
+		let realNumberCount = 0 // 实数部分的长度
+		if (maxValueString.indexOf('.') === -1) {
+			realNumberCount = maxValueString.length
+		} else {
+			realNumberCount = maxValueString.split('.')[0].length
+		}
+		return [maxValue, realNumberCount]
 	}
 	/**
 	 * 计算 canvas 画布的大小
@@ -109,10 +169,13 @@ class Chart {
 	 */
 	drawAxis(start: number[], end: number[]) {
 		this.context.beginPath()
+		this.context.strokeStyle = 'black'
+		this.context.lineWidth = 1
+
+		this.context.lineCap = 'round'
 		this.context.moveTo(start[0], start[1])
 		this.context.lineTo(end[0], end[1])
 		this.context.stroke()
-		this.context.lineCap = 'round'
 		this.context.closePath()
 	}
 	/**
@@ -126,11 +189,11 @@ class Chart {
 	 * @param {string} [fontStyle='Arial'] 字体
 	 * @memberof Chart
 	 */
-	drawFont(text: string, x: number, y: number, color: string = '#333', size: number = 16, fontStyle: string = 'Arial') {
+	drawText(text: string, x: number, y: number, color: string = '#333', size: number = 16, fontStyle: string = 'Arial') {
 		this.context.save()
 		this.context.font = `${size}px ${fontStyle}`
 		this.context.fillStyle = color
-		this.context.fillText(text, x, y)
+		this.context.fillText(text, x, y + (size / 2))
 	}
 	/**
 	 * 画图表矩形
